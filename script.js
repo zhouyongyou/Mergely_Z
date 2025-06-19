21ac1650b573baf63c4be13fafd595cf074267dc
ZhouYongyou <128128010+zhouyongyou@users.noreply.github.com>
2025-06-19 18:41:52 +0800

    Add CodeMirror syntax highlighting and language selector

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // 取得 DOM 元素
 const lhsTextarea = document.getElementById('lhs');
 const rhsTextarea = document.getElementById('rhs');
+const languageSelect = document.getElementById('language-select');
+
+// 使用 CodeMirror 取代 textarea
+const lhsEditor = CodeMirror.fromTextArea(lhsTextarea, {
+  lineNumbers: true,
+  lineWrapping: true,
+  mode: 'javascript',
+});
+
+const rhsEditor = CodeMirror.fromTextArea(rhsTextarea, {
+  lineNumbers: true,
+  lineWrapping: true,
+  mode: 'javascript',
+});
 
 // 初始化 Mergely，啟用行號與自動換行
 const mergely = new Mergely('#compare', {
@@ -10,13 +24,20 @@ const mergely = new Mergely('#compare', {
 
 // 更新比對結果的函式
 function updateDiff() {
-  mergely.lhs(lhsTextarea.value);
-  mergely.rhs(rhsTextarea.value);
+  mergely.lhs(lhsEditor.getValue());
+  mergely.rhs(rhsEditor.getValue());
 }
 
 // 首次載入完成後即執行比對
 mergely.once('updated', updateDiff);
 
 // 在輸入框輸入時即時比對
-lhsTextarea.addEventListener('input', updateDiff);
-rhsTextarea.addEventListener('input', updateDiff);
+lhsEditor.on('change', updateDiff);
+rhsEditor.on('change', updateDiff);
+
+// 切換語言時同步更換語法模式
+languageSelect.addEventListener('change', () => {
+  const mode = languageSelect.value;
+  lhsEditor.setOption('mode', mode);
+  rhsEditor.setOption('mode', mode);
+});
